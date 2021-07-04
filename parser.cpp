ae681fc2897cfd2d0b9561b0a5c4a3629f30993a
Czterolistny <strzeciak3@gmail.com>
2021-07-04 10:37:08 +0200

    Add get_arg() and get_arg_cnt() function to gain parser arguments and add error handling for it; Code refactoring;

--- parser.cpp
+++ parser.cpp
@@ -15,7 +15,7 @@ public:
 		OPTIONAL = 1
 	};
 	
-	void print_help(void){
+	void print_help(void) const{
 		std::cout<<"Usage: "<< program_name <<" ";
 		for(auto &pair : args_info){
 			std::cout<< pair.first;
@@ -53,7 +53,7 @@ public:
 		return add_opt(opt, opt_arg_cnt, opt_arg_cnt, argType);
 	}
 	
-	int get_arg_cnt(char **args, int arg_len){
+	int get_arg_cnt(char **args, int arg_len) const{
 		for(int i = 0; i < arg_len; ++i){
 			if( args[i][0] == '-'){
 				return i;
@@ -101,25 +101,41 @@ public:
 		return 0;
 	}
 
-	std::vector< std::vector<std::string> > & get_parser_ctx(){
+	std::vector< std::vector<std::string> > const & get_parser_ctx() const{
 		return arg_vec;
 	}
 
-	void print_args(){
-		for(auto &arg: arg_vec){
+	void print_args() const{
+		for(auto const &arg: arg_vec){
 			for(auto it = arg.begin(); it != arg.end(); ++it){
 				std::cout<< *it <<" ";
 			}
 			std::cout<<std::endl;
 		}
 	}
-
+	int get_arg(const std::string opt, std::string &arg, int nmb){
+		if(nmb == 0){
+			return -1;
+		}
+		auto it = find_opt_it(opt.c_str() );
+		if( it->size() > nmb ){
+			arg = it->at(nmb);
+			return 0;
+		}else{
+			std::cerr<<"get_arg() error..."<<"   "<< opt <<"["<< nmb <<"] out of range"<<std::endl;
+			return -1;
+		}
+	}
+	int get_arg_cnt(std::string &opt){
+		auto it = find_opt_it( opt.c_str() );
+		return it->size() - 1;
+	}
 private:
 	typedef std::vector< std::vector<std::string> >::iterator pars_it;
 	typedef std::vector< std::pair<std::string, std::vector<int> >> args_info_type; 
 
 	std::string program_name;
-	pars_it find_opt_it(char *args){
+	pars_it find_opt_it(const char *args){
 		for(auto it = arg_vec.begin(); it != arg_vec.end(); ++it){
 			if( strcmp( it->at(0).c_str(), args ) == 0 ){
 				return it;
@@ -127,8 +143,8 @@ private:
 		}
 		return arg_vec.end();
 	}
-	int check_args_cnt(pars_it it, args_info_type &args_info, int arg_cnt){
-		for(auto &pair : args_info){
+	int check_args_cnt(pars_it it, args_info_type &args_info, int arg_cnt) const{
+		for(auto const &pair : args_info){
 			if( pair.first == it->at(0) ){
 				if( (arg_cnt > pair.second.at(1)) || (arg_cnt < pair.second.at(0)) ){
 					std::cerr<< "Argument error..."<<"  "<< it->at(0) \
@@ -139,10 +155,10 @@ private:
 		}
 		return 0;
 	}
-	int check_all_mandatory_args(){
-		for(auto &arg : arg_vec){
+	int check_all_mandatory_args() const{
+		for(auto const &arg : arg_vec){
 			if( arg.size() < 2){
-				for(auto &pair : args_info){
+				for(auto const &pair : args_info){
 					if( pair.first == arg.at(0) ){
 						if( (ARG_TYPE) pair.second.at(2) == ARG_TYPE::MANDATORY ){
 							std::cerr<<"Argument error..."<<"  "<<"["<< arg.at(0) <<"]"<<" is not given" <<std::endl;
@@ -165,33 +181,38 @@ int main(int argc, char **argv)
 	
 	if( parser.add_opt("-out", 1) || \
 		parser.add_opt("-in", 2) || \
-		parser.add_opt("-xxd", 1, 2, Parser::ARG_TYPE::OPTIONAL) == -1) \
+		parser.add_opt("-aux", 1, 2, Parser::ARG_TYPE::OPTIONAL) == -1){
 			return -1;
+		}
 	
 	if( parser.add_args(&argv[0], argc - 1) == -1){
 		return -1;
 	}
 
 	std::string opt1_arg1, opt2_arg1, opt2_arg2;
-	auto parser_process = [&opt1_arg1, &opt2_arg1, &opt2_arg2](auto ctx)
+	int arg_err = 0;
+	auto parser_process = [&parser, &opt1_arg1, &opt2_arg1, &opt2_arg2, &arg_err](auto ctx)
 	{
 		std::string opt = ctx.at(0);
 		if( opt == "-out" ){
-			opt1_arg1 = ctx.at(1);
-
+			arg_err += parser.get_arg(opt, opt1_arg1, 1);
 		}else if( opt == "-in" ){
-			opt2_arg1 = ctx.at(1);
-			opt2_arg2 = ctx.at(2);
-		}else if( opt == "-xxd" ){
-			
+			arg_err += parser.get_arg(opt, opt2_arg1, 1);
+			arg_err += parser.get_arg(opt, opt2_arg2, 2);
+		}else if( opt == "-aux" ){
+			int cnt;
+			if( (cnt = parser.get_arg_cnt(opt)) ){
+				std::cout<< opt <<" arg cnt "<< cnt <<std::endl;
+			}
 		}else{
 			std::cout<<"Something gone wrong"<<std::endl;
 		}
-
 	 };
-
 	auto parse_ctx = parser.get_parser_ctx();
 	for_each(parse_ctx.begin(), parse_ctx.end(), parser_process);
+	if(arg_err < 0){
+		return -1;
+	}
 
 	std::cout<<"-out: "<< opt1_arg1 <<std::endl;
 	std::cout<<"-in: "<< opt2_arg1 <<" "<< opt2_arg2 <<std::endl;
